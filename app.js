6db8c4c037828d17271d260b320931bfc27b9399
koraresu <raelcorrales@gmail.com>
2016-08-09 15:37:06 -0500

    Movido Newrelic al inicio

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require('newrelic');
+
 var express      = require('express');
 var path         = require('path');
 var favicon      = require('serve-favicon');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-require('newrelic');
+
 
 var routes       = require('./routes/index');
 var profile      = require('./routes/profile');
