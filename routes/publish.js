89601d03bc2360121d35325f1a4e129ba34c6097
koraresu <raelcorrales@gmail.com>
2016-11-01 20:31:22 -0600

    Phones

--- routes/publish.js
+++ routes/publish.js
@@ -385,7 +385,7 @@ router.post('/get/news/show', multipartMiddleware, function(req, res){
 				History.findOne({ _id: news_id }).populate('profile_id').populate('de_id').exec(function(errHistory,historyData){
 
 					var d = format.news(historyData, historyData.profile_id, historyData.de_id);
-					Generalfunc.response(200, data, function(response){
+					Generalfunc.response(200, d, function(response){
 						res.json(response);
 					});
 				});
