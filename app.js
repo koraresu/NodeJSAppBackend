5a403a80c7df7e5107c6966603208ee04f583c2b
koraresu <raelcorrales@gmail.com>
2016-11-15 14:05:19 -0600

    GPS

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ gps.on('connection', function(socket){
 
   socket.on('setlocation', function(data){
     console.log("CLIENT GPS:"+clientGPS.length);
-
+    console.log(clientGPS);
     if(data == undefined || data == null){
       socket.emit('getlocation',{ message: "GET DATA UNDEFINED OR NULL"});
     }else{
