eaac87e26ff1a101d4a9a0f8a5b2b0cb23bd2011
koraresu <raelcorrales@gmail.com>
2017-01-16 14:31:03 -0600

    Err Conversation

--- routes/chat.js
+++ routes/chat.js
@@ -83,6 +83,8 @@ var apnProvider = new apn.Provider(options);
 								}
 							}).populate('profiles').populate('message').sort({ updatedAt: -1 }).exec(function(err, conversationData){
 								if(!err && conversationData){
+									console.log("Conversation Data:");
+									console.log( conversationData );
 									async.map(conversationData, function(item, ca){
 										if(item.profiles > 1){
 											var ajeno = Generalfunc.profile_ajeno(profileData._id, item.profiles);
