35ec7ad164d1f202927364babf4a7bc015055152
koraresu <raelcorrales@gmail.com>
2017-01-31 19:47:27 -0600

    Profile to Socket

--- routes/network.js
+++ routes/network.js
@@ -29,6 +29,7 @@ var Notification = model.notification;
 var Feedback     = model.feedback;
 var Conversation = model.conversation;
 var Message      = model.message;
+var Online       = model.online;
 var City         = model.city;
 var State        = model.state;
 var Country      = model.country;
