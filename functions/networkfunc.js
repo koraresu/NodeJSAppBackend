8d586887d370116638818657f4c51a8086cca491
koraresu <raelcorrales@gmail.com>
2017-02-21 14:10:04 -0600

    Network

--- functions/networkfunc.js
+++ functions/networkfunc.js
@@ -424,7 +424,7 @@ function create_notificacion_recomendacion(data, callback){
 }
 function new_friend(profileData, profileAnotherData, success, fail){
 	var network = new Network({
-		accepted: accepted,
+		accepted: false,
 		profiles: [
 		profileData._id,
 		profileAnotherData._id
