b6bfdcb6872fa20fadb9dc7e4bc347a1ed247ffb
koraresu <raelcorrales@gmail.com>
2017-03-29 19:39:08 -0600

    Invitación GPS

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ gps.on('connection', function(socket){
 
     gpsrouter.invite(guid, public_id, function(data, gpsData){
       var s = gpsData.socket;
-      console.log( "Socket Broadcast:" + gs );
+      console.log( "Socket Broadcast:" + s );
       io.to('/#' + s).emit('gps_invite',data);
     }, function(data){
       socket.emit('gps_invited',data);
