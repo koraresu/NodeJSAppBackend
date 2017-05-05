35539f49cb0c555954e035ee7dc1d1287f095d50
koraresu <raelcorrales@gmail.com>
2017-05-04 19:41:24 -0500

    Forgot

---
+++ public/javascripts/forgot.js
@@ -0,0 +1,36 @@
+$(document).on('ready', function(){
+	$.validator.addMethod("atLeastOneLowercaseLetter", function (value, element) {
+		return this.optional(element) || /[a-z]+/.test(value);
+	}, "Debe tener mínimo una letra minúscula");
+	$.validator.addMethod("atLeastOneUppercaseLetter", function (value, element) {
+		return this.optional(element) || /[A-Z]+/.test(value);
+	}, "Debe tener mínimo una letra mayúscula");
+	$.validator.addMethod("atLeastOneNumber", function (value, element) {
+		return this.optional(element) || /[0-9]+/.test(value);
+	}, "Debe tener mínimo un número");
+
+	$('.forgot form').validate({
+		rules: {
+			password: {
+				required: true,
+				atLeastOneLowercaseLetter: true,
+				atLeastOneUppercaseLetter: true,
+				atLeastOneNumber: true,
+				minlength: 8,
+			},
+			password_again: {
+				equalTo: "#password"
+			}
+		},
+		messages: {
+			password: {
+				required: 'Requerido',
+				minlength: "Mínimo de 8 caracteres."
+			},
+			password_again: {
+				required: 'Requerido',
+
+			}
+		}
+	});
+});
